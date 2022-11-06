ca9cf4a89886ce449600e8ac41d7e201b89c3b44
Hristo Mavrodiev <h.m.mavrodiev@gmail.com>
2022-11-06 17:50:04 +0200

    removed prints

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,6 @@ import (
 	"crypto/internal/gate"
 	"crypto/internal/server"
 	"crypto/internal/stex"
-	"fmt"
 )
 
 var (
@@ -21,9 +20,6 @@ func main() {
 
 	go arbitrage.ExecuteArbitrage(&gatePriceInfo, &stexPriceInfo, &ArbitrageResponseList, &GateToStex, &StexToGate)
 	go client.StartPlaftormsClient(&gatePriceInfo, &stexPriceInfo)
-	fmt.Println(ArbitrageResponseList)
-	fmt.Println("HI")
-
 	r := server.Server(&gatePriceInfo, &stexPriceInfo, &ArbitrageResponseList)
 	r.Run(":8080")
 
